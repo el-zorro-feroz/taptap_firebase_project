a1d548c2482ca725112529a47a14e5890cc1a24a
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-09-13 18:57:27 +0300

    feat!: update user info entity

--- lib/src/domain/entities/user_info.dart
+++ lib/src/domain/entities/user_info.dart
@@ -1,1 +1,10 @@
-class UserInfo {}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class UserInfo {
+  final String name;
+  final String model;
+
+  UserInfo({
+    required this.name,
+    required this.model,
+  });
+}
